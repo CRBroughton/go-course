f5b69de3ce5fa2bfa784475cd0324161b2cc942e
CRBroughton <crbroughton@posteo.uk>
2022-06-18 10:09:28 +0100

    feat(ticket-booking): create for loop for flitering and showing only first names

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
@@ -36,6 +39,11 @@ func main() {
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 		fmt.Printf("%v ticket remaining for %v\n", remainingTickets, conferenceName)
 
-		fmt.Printf("These are all our bookings: %v\n", bookings)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("These are all our bookings: %v\n", firstNames)
 	}
 }
